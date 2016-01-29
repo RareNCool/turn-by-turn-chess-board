348ad0fa587f1a1481fffedee58436a17951b2c8
StillLearnin <melvinray@abcrun.com>
2016-01-28 23:25:17 -0500

    basic play sort of working

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
+var ground;
+var chess = new Chess();
+
 (function() {
     var fen;
+    var orientation;
     if(typeof(Storage) !== "undefined") {
         fen = localStorage.getItem("fen");
+        orientation = localStorage.getItem("orientation");
     } else {
         alert("Sorry! No Web Storage support..");
     }
-    var ground;
-    var chess = new Chess();
+
     var onMove = function(orig, dest) {
-    chess.move({from: orig, to: dest});
-    ground.set({
-    turnColor: chessToColor(chess),
-    movable: {
-        color: chessToColor(chess),
-        dests: chessToDests(chess)
-    }
-    });
-    // ground.toggleOrientation();
-    console.log(ground.getFen());
-    localStorage.setItem("fen", ground.getFen());
+        chess.move({from: orig, to: dest});
+        ground.set({
+            turnColor: chessToColor(chess),
+            movable: {
+                color: chessToColor(chess),
+                dests: chessToDests(chess)
+            }
+        });
+        logMove(JSON.stringify({ "From": orig, "To": dest}));
     };
+
+    if (!orientation)
+        orientation = "white";
+
     ground = Chessground(document.getElementById('chessBoard'), {
-    orientation: "white",
-    viewOnly: false,
-    turnColor: 'white',
-    animation: {
-    duration: 500
-    },
-    movable: {
-    free: false,
-    color: chessToColor(chess),
-    premove: true,
-    dests: chessToDests(chess),
-    events: {
-        after: onMove
-    }
-    },
-    drawable: {
-    enabled: true
-    }
+        orientation: orientation,
+        viewOnly: false,
+        turnColor: 'white',
+        animation: {
+            duration: 500
+        },
+        movable: {
+            free: true,
+            color: chessToColor(chess),
+            premove: true,
+            dests: chessToDests(chess),
+            events: {
+                after: onMove
+            }
+        },
+        drawable: {
+            enabled: true
+        }
     });
     if (fen)
-    ground.set({fen: fen});
-    //   window.cg6 = ground;
-    //   ground.move("e2", "e4");
+         ground.set({fen: fen});
 })();
 
 function chessToDests(chess) {
@@ -59,20 +63,45 @@ function chessToColor(chess) {
     return (chess.turn() == "w") ? "white" : "black";
 }
 
-function loadFen() {
-    console.log(document.getElementById('fenInput').value);
-    localStorage.setItem("fen", document.getElementById('fenInput').value);
+function logMove(move) {
+    var fen = ground.getFen();
+    console.log(fen);
+    localStorage.setItem("fen", fen);
+    setValue("currentFen", fen);
+    setValue("lastMove", move);
+    setValue("moveToPlay", "");
+    document.getElementById("lastMove").select();
+    try {
+        var successful = document.execCommand('copy');
+        //var msg = successful ? 'Successfully copied' : 'Unable to copy';
+        //alert(msg + ' move to clipboard');
+    } catch (err) {
+        alert('Oops, unable to copy move to clipboard automatically! You will have to do it yourself');
+    }
+}
+
+function LoadFen() {
+    var fen = getValue("fenToLoad");
+    console.log(fen);
+    localStorage.setItem("fen", fen);
     location.reload();
 }
 
-function playMove() {
-    var chess = new Chess();
-    var orig = document.getElementById('moveOrig').value;
-    var dest = document.getElementById('moveDest').value;
-    chess.move({from: orig, to: dest});
+function PlayMove() {
+    var move = JSON.parse(getValue("moveToPlay"));
+    ground.move(move.From, move.To);
+    logMove(JSON.stringify(move));
 }
 
 function resetGame() {
     localStorage.removeItem("fen");
     location.reload();
+}
+
+function getValue(id) {
+    return document.getElementById(id).value;
+}
+
+function setValue(id, value) {
+    document.getElementById(id).value = value;
 }
